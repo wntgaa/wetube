12e263fe26444cdf9e682e8e750ff72b687eac3e
wntgaa <wntgaa@kakao.com>
2021-01-28 01:02:05 +0900

    제작 중 백업용 업로드

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Wetube
 
 cloning Youtube with Vanilla and NodeJS
+
+##Pages:
+
+-  [] Home
+-  [] Join
+-  [] Login
+-  [x] Search
+-  [] UserDetail
+-  [] EditProfile
+-  [] Changepassword
+-  [] Upload
+-  [] VideoDetail
+-  [] EditVideo
--- app.js
+++ app.js
@@ -4,18 +4,20 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
+import {localMiddleware} from "./middlewares";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from"./routes";
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
-
 app.use(morgan("dev"));
+app.use(localMiddleware);
 
+app.set("view engine", "pug");
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter);
 app.use(routes.videos,videoRouter);
