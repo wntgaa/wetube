bc1e6e6341e8c8a856f544ecbc4c159ebd9cc29d
wntgaa <wntgaa@kakao.com>
2021-02-05 01:44:38 +0900

    eslint 연결 및 비디오 정렬, 아이디별 비디오 연결, 비디오 수정 기능 구현

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try{
-    const videoin = await Video.find({});
+    const videoin = await Video.find({}).sort({_id:-1});
     res.render("home", { pageTitle: "Home", videoin });
   } catch(error){
     console.log(error);
@@ -33,11 +33,51 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: video.title, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
   
-export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect(routes.home);
+};
--- routes.js
+++ routes.js
@@ -18,7 +18,7 @@ const VIDEOS="/videos";
 const UPLOAD="/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO="/:id/edit";
-const DElETE_VIDEO="/:id/delete";
+const DELETE_VIDEO="/:id/delete";
 
 const routes = {
     home: HOME,
@@ -45,8 +45,20 @@ const routes = {
           return VIDEO_DETAIL;
         }
       },
-    editVideo: EDIT_VIDEO,
-    deleteVideo: DElETE_VIDEO
+      editVideo: id => {
+        if (id) {
+          return `/videos/${id}/edit`;
+        } else {
+          return EDIT_VIDEO;
+        }
+      },
+      deleteVideo: id => {
+        if (id) {
+          return `/videos/${id}/delete`;
+        } else {
+          return DELETE_VIDEO;
+        }
+      }
 };
 
 export default routes;
